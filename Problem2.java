39e34512178804bf139b9dcb287488368baeb6dd
Brain17 <b.daminov@newuu.uz>
2026-01-30 12:49:47 +0500

    Add greeting program in Problem2.java

---
+++ Problem2.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Main {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    System.out.print("Please enter your name: ");
+    String name = scanner.nextLine();
+    System.out.printf("HELLO, %s!%n", name);
+  }
+}
