c3d6893168d428f12f24cd3b004e26c9ca7e5c3b
Brain17 <b.daminov@newuu.uz>
2026-01-30 12:32:03 +0500

    Add Main class to print user details

---
+++ Problem1.java
@@ -0,0 +1,6 @@
+public class Main {
+  public static void main(String[] args) {
+    System.out.printf("Name: %s%nAddress: %s%nStreet: %s%nCountry: %s%n",
+            "Daminov Bobur", "Samarkand city", "Bodomzor 12/1", "Uzbekistan");
+  }
+}
